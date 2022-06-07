e9897e58a5c7fd1dc81fa76c0dc810f39dd6198e
--global <geronimosalaiba@gmail.com>
2022-06-07 10:15:13 -0300

    expres

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-console.log('hola')
+//requerimos express
+const express = require('express');
+//requioromo spath para usar el metod sendfile y pode mostrar los archivos
+const path = require('path');
+//funcionalidades de express
+const app = express();
+//Tiene el metodo listen exprees para poneer a correr un servidor
+app.listen(3000,()=>{
+    console.log("servidor corriendo")
+});
+//Pedir que me muetre con get
+app.get("/",function(req,res){
+    res.send("Hola Gero")
+});
+//metodo para cada ruta
+app.get("/home",function(req,res){
+    res.send("wlecome")
+});
+//METODO SENDFILE() nos permitira enviar un archio al navegador para interpretarlo
+app.get("/gero",function(req,res){
+    let htmlPath = path.resolve(__dirname,'./views/index.html')
+     res.sendFile(htmlPath);
+})
+
